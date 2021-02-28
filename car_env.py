c7dc2be97ced6538ea18dfd680057f8800eed3f1
nerkulec <nerkulec@github.com>
2021-02-28 17:36:23 +0100

    drawing trajectories

--- car_env.py
+++ car_env.py
@@ -8,15 +8,16 @@ import math
 
 from car import Car, max_vel, max_acc
 from racing_track import Map, tile_width
+from util import Vec
 
 class Spec: # remove when env properly registered
   def __init__(self, id):
     self.id = id
 
 class CarEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+  metadata = {'render.modes': ['human', 'trajectories']}
 
-  def __init__(self, file_name = 'maps/map1.txt', num_rays = 12):
+  def __init__(self, file_name = 'maps/map1.txt', num_rays = 12, draw_rays = True):
     super().__init__()
     self.action_space = spaces.Box(-1, 1, shape=(2,), dtype=np.float32)
     self.map = Map(file_name)
@@ -33,15 +34,17 @@ class CarEnv(gym.Env):
     )
     self.window = None
     self.spec = Spec('RLCar-v0')
+    self.color = None
+    self.trajectories = [[]]
+    self.heatmap = []
+    self.batch = None
+    self.draw_rays = draw_rays
+    self.opacity = None
     
   def get_obs(self):
-    if hasattr(self, "batch"):
-      batch = self.batch
-    else:
-      batch = None
     obs = [
       np.array([self.car.vel.x, self.car.vel.y]),
-      self.map.get_closest(self.car.pos, '#', rays=self.num_rays, batch=batch),
+      self.map.get_closest(self.car.pos, '#', rays=self.num_rays, batch=self.draw_rays and self.batch or None),
       # self.map.get_closest(self.car.pos, 'O'),
       # self.map.get_closest(self.car.pos, 'C'),
       self.map.get_closest(self.car.pos, 'M', rays=self.num_rays)
@@ -51,7 +54,13 @@ class CarEnv(gym.Env):
   def step(self, action):
     action = action*max_acc
     car = self.car
+    if self.color is not None:
+      prev_pos = Vec(car.pos.x, car.pos.y)
     car.update(action, self.ground)
+    if self.color is not None and self.batch is not None:
+      line = shapes.Line(prev_pos.x*tile_width, prev_pos.y*tile_width, car.pos.x*tile_width, car.pos.y*tile_width, color=self.color, batch=self.batch)
+      line.opacity = self.opacity or 63
+      self.trajectories[-1].append(line)
     self.ground = self.map[car.pos]
     obs = self.get_obs()
     # obs = np.array([car.pos.x, car.pos.y, car.vel.x, car.vel.y])*np.array([1, 1, 1/max_vel, 1/max_vel])
@@ -77,6 +86,9 @@ class CarEnv(gym.Env):
     if done and self.ground != 'M':
       reward -= math.sqrt((car.pos.x-self.map.M.x)**2+(car.pos.y-self.map.M.y)**2)
     
+    if done:
+      self.trajectories.append([])
+    
     return obs, reward, done, {}
     
   def reset(self):
@@ -84,23 +96,37 @@ class CarEnv(gym.Env):
     self.ground = ' '
     self.steps = 0
     return self.get_obs()
+  
+  def set_color(self, color):
+    self.color = color    
     
   def render(self, mode='human', close=False):
-    if self.window is None:
-      self.window = pyglet.window.Window(self.map.width*tile_width, self.map.height*tile_width)
-      glTranslatef(-1, 1, 0)
-      glScalef(2/self.map.width/tile_width, -2/self.map.height/tile_width, 1)
-      self.batch = pyglet.graphics.Batch()
-      self.circle = shapes.Circle(self.car.pos.x*tile_width, self.car.pos.y*tile_width, tile_width/4, color=(255, 128, 128), batch=self.batch)
-      self.map.draw(self.batch)
-    self.circle.x = self.car.pos.x*tile_width
-    self.circle.y = self.car.pos.y*tile_width
-    self.window.clear()
-    # Draw board
-    self.batch.draw()
-    # Draw car
-    self.circle.draw()
-    self.window.flip()
+    if mode == 'human':
+      if self.window is None:
+        self.window = pyglet.window.Window(self.map.width*tile_width, self.map.height*tile_width)
+        glTranslatef(-1, 1, 0)
+        glScalef(2/self.map.width/tile_width, -2/self.map.height/tile_width, 1)
+        self.batch = pyglet.graphics.Batch()
+        self.circle = shapes.Circle(self.car.pos.x*tile_width, self.car.pos.y*tile_width, tile_width/4, color=(255, 128, 128), batch=self.batch)
+        self.map.draw(self.batch)
+      self.circle.x = self.car.pos.x*tile_width
+      self.circle.y = self.car.pos.y*tile_width
+      self.window.clear()
+      # Draw board
+      self.batch.draw()
+      # Draw car
+      self.circle.draw()
+      self.window.flip()
+    if mode == 'trajectories':
+      if self.window is None:
+        self.window = pyglet.window.Window(self.map.width*tile_width, self.map.height*tile_width)
+        glTranslatef(-1, 1, 0)
+        glScalef(2/self.map.width/tile_width, -2/self.map.height/tile_width, 1)
+        self.batch = pyglet.graphics.Batch()
+        self.map.draw(self.batch)
+      self.window.clear()
+      self.batch.draw()
+      self.window.flip()
     
   def close(self):
     super().close()
--- test.py
+++ test.py
@@ -2,13 +2,12 @@ from car_env import CarEnv
 import numpy as np
 from util import Vec
 
-env = CarEnv(file_name = 'maps/maptest.txt')
-
+env = CarEnv(file_name = 'maps/map3.txt', draw_rays=False)
+env.color = (255,0,0)
 while True:
     done = False
     state = env.reset()
     while not done:
-        env.render()
         action = env.action_space.sample()
         state, reward, done, _ = env.step(action)
-    env.render()
+        env.render(mode='trajectories')
