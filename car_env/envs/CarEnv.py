b9043ad5e6c58707057a574cc67ed8646f996f77
Bartosz Brzoza <bartoszbrzoza1@gmail.com>
2021-03-21 13:40:16 +0100

    fix tabs

--- car_env/envs/CarEnv.py
+++ car_env/envs/CarEnv.py
@@ -151,131 +151,131 @@ max_acc = 0.02
 
 class Car:
   def __init__(self, pos=None):
-      if pos is None:
-          self.pos = Vec(1.5, 1.5)
-      else:
-          self.pos = pos
-      self.vel = Vec(0, 0)
+    if pos is None:
+        self.pos = Vec(1.5, 1.5)
+    else:
+        self.pos = pos
+    self.vel = Vec(0, 0)
   
   def update(self, acc, field):
-      acc = Vec(acc)
-      acc.limit(max_acc)
-      if field == 'O':
-          acc += Vec(np.random.normal(scale=oil_sigma, size=2))
-      self.vel += acc
-      self.vel.limit(max_vel)
-      self.pos += self.vel
+    acc = Vec(acc)
+    acc.limit(max_acc)
+    if field == 'O':
+        acc += Vec(np.random.normal(scale=oil_sigma, size=2))
+    self.vel += acc
+    self.vel.limit(max_vel)
+    self.pos += self.vel
 
 class Vec:
   def __init__(self, a, b=None):
-      self.x, self.y = a if b is None else (a, b)
+    self.x, self.y = a if b is None else (a, b)
   
   def __add__(self, other):
-      return Vec(self.x+other.x, self.y+other.y)
+    return Vec(self.x+other.x, self.y+other.y)
   
   def __iadd__(self, other):
-      self.x+=other.x
-      self.y+=other.y
-      return self
+    self.x+=other.x
+    self.y+=other.y
+    return self
   
   def __sub__(self, other):
-      return Vec(self.x-other.x, self.y-other.y)
+    return Vec(self.x-other.x, self.y-other.y)
   
   def __neg__(self):
-      return Vec(-self.x, -self.y)
+    return Vec(-self.x, -self.y)
   
   def mag(self):
-      return math.sqrt(self.x**2 + self.y**2)
+    return math.sqrt(self.x**2 + self.y**2)
   
   def limit(self, r):
-      m = self.mag()
-      if m > r:
-          self.x *= r/m
-          self.y *= r/m
+    m = self.mag()
+    if m > r:
+        self.x *= r/m
+        self.y *= r/m
   
   def __eq__(self, other):
-      return other is not None and self.x == other.x and self.y == other.y
+    return other is not None and self.x == other.x and self.y == other.y
   
   def copy(self):
-      return Vec(self.x, self.y)
+    return Vec(self.x, self.y)
   
   def __repr__(self):
-      return f"({self.x}, {self.y})"
+    return f"({self.x}, {self.y})"
   
   def __lt__(self, other):
-      return self.x<other.x or (self.x==other.x and self.y < other.y)
+    return self.x<other.x or (self.x==other.x and self.y < other.y)
   
   def __hash__(self):
-      return hash((self.x, self.y))
+    return hash((self.x, self.y))
 
 class Map:
-  def __init__(self, file_name = 'maps/map1.txt'):
-      board = []
-      with open(file_name, 'r') as f:
-          for line in f:
-              board.append([c for c in line[:-1]])
-      self.width = len(board[0])
-      self.height = len(board)
-      self.board = board
-      for y in range(self.height):
-          for x in range(self.width):
-              v = Vec(x, y)
-              if self[v] == 'M':
-                  self.M = v + Vec(0.5, 0.5)
+  def __init__(self, file_name = 'maps/map.txt'):
+    board = []
+    with open(file_name, 'r') as f:
+      for line in f:
+        board.append([c for c in line[:-1]])
+    self.width = len(board[0])
+    self.height = len(board)
+    self.board = board
+    for y in range(self.height):
+      for x in range(self.width):
+        v = Vec(x, y)
+        if self[v] == 'M':
+          self.M = v + Vec(0.5, 0.5)
 
   def __getitem__(self, pos):
-      try:
-          return self.board[math.floor(pos.y)][math.floor(pos.x)]
-      except:
-          return '#'
+    try:
+        return self.board[math.floor(pos.y)][math.floor(pos.x)]
+    except:
+        return '#'
 
   def __setitem__(self, pos, val):
-      self.board[pos.y][pos.x] = val
+    self.board[pos.y][pos.x] = val
 
   def __contains__(self, pos):
-      return 0 <= pos.x < self.width and\
-              0 <= pos.y < self.height and\
-              self[pos] != '#'
+    return 0 <= pos.x < self.width and\
+            0 <= pos.y < self.height and\
+            self[pos] != '#'
               
   def get_closest(self, pos, field = '#', jump = 0.2, num_steps = 11, rays = 12, batch = None):
+    if batch is not None:
+        self.rays = []
+    closest = np.zeros(rays)
+    for i in range(rays):
+      c = math.cos(i*2*math.pi/rays)
+      s = math.sin(i*2*math.pi/rays)
+      for j in range(1, num_steps+1):
+        ray = pos + Vec(c*j*jump, s*j*jump)
+        closest[i] = j
+        if self[ray] == field:
+          break
       if batch is not None:
-          self.rays = []
-      closest = np.zeros(rays)
-      for i in range(rays):
-          c = math.cos(i*2*math.pi/rays)
-          s = math.sin(i*2*math.pi/rays)
-          for j in range(1, num_steps+1):
-              ray = pos + Vec(c*j*jump, s*j*jump)
-              closest[i] = j
-              if self[ray] == field:
-                  break
-          if batch is not None:
-              coef = math.floor(255*(num_steps-j)/(num_steps-1))
-              self.rays.append(shapes.Line(
-                  pos.x*tile_width, pos.y*tile_width, ray.x*tile_width, ray.y*tile_width, 1,
-                  color=(coef, (255-coef)//2, 0), batch=batch))
-      return (num_steps-closest)/(num_steps-1)
+        coef = math.floor(255*(num_steps-j)/(num_steps-1))
+        self.rays.append(shapes.Line(
+            pos.x*tile_width, pos.y*tile_width, ray.x*tile_width, ray.y*tile_width, 1,
+            color=(coef, (255-coef)//2, 0), batch=batch))
+    return (num_steps-closest)/(num_steps-1)
 
   def print(self):
-      for y in range(self.height):
-          for x in range(self.width):
-              print(self.board[y][x], end='')
-          print()
+    for y in range(self.height):
+      for x in range(self.width):
+        print(self.board[y][x], end='')
+      print()
   
   def draw(self, batch):
-      self.rects = []
-      for y in range(self.height):
-          for x in range(self.width):
-              g = self[Vec(x, y)]
-              if g == ' ':
-                  c = (0, 0, 0)
-              elif g == '#':
-                  c = (128, 128, 128)
-              elif g == 'C':
-                  c = (140, 120, 120)
-              elif g == 'O':
-                  c = (40, 26, 13)
-              elif g == 'M':
-                  c = (128, 128, 256)
-              rect = shapes.Rectangle(x*tile_width, y*tile_width, tile_width, tile_width, color=c, batch=batch)
-              self.rects.append(rect)
+    self.rects = []
+    for y in range(self.height):
+      for x in range(self.width):
+        g = self[Vec(x, y)]
+        if g == ' ':
+          c = (0, 0, 0)
+        elif g == '#':
+          c = (128, 128, 128)
+        elif g == 'C':
+          c = (140, 120, 120)
+        elif g == 'O':
+          c = (40, 26, 13)
+        elif g == 'M':
+          c = (128, 128, 256)
+        rect = shapes.Rectangle(x*tile_width, y*tile_width, tile_width, tile_width, color=c, batch=batch)
+        self.rects.append(rect)
